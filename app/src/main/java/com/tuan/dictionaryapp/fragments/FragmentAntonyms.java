85846c65acc5b4e8eeb5dde164613eb2a10c0caa
tua2 <18521606@gm.uit.edu.vn>
2022-06-07 23:22:50 +0700

    dictionary app

---
+++ app/src/main/java/com/tuan/dictionaryapp/fragments/FragmentAntonyms.java
@@ -0,0 +1,46 @@
+package com.tuan.dictionaryapp.fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.tuan.dictionaryapp.R;
+import com.tuan.dictionaryapp.WordMeaningActivity;
+
+public class FragmentAntonyms extends Fragment {
+
+
+    public FragmentAntonyms() {
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+// Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_definition, container, false);//Inflate Layout
+
+        Context context = getActivity();
+
+        TextView text = (TextView) view.findViewById(R.id.textview_d);//Find textView Id
+
+        String example = ((WordMeaningActivity) context).antonyms;
+        text.setText(example);
+
+        if (example == null) {
+            text.setText("No Example found");
+        }
+
+
+        return view;
+
+
+    }
+}
+
